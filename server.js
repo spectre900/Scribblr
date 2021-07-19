b0a235736f936c43def3aa028b33f083c17023a2
spectre900 <nayakpratham0@gmail.com>
2021-07-20 03:16:51 +0530

    added download api

--- server.js
+++ server.js
@@ -11,17 +11,31 @@ app.use(formidable());
 app.post('/api/save', (req, res) => {
   const imagePath = req.files.img.path;
   const imageName = req.files.img.name;
+  if (!fs.existsSync('./images')){
+    fs.mkdirSync('./images');
+  }
   fs.writeFileSync('images/'+imageName, fs.readFileSync(imagePath));
   res.sendStatus(200);
 });
 
 app.get('/api/show', (req, res) => {
-  fs.readdir('./images', (err, files) => {
-    if(err){
-      res.sendStatus(404);
-    }
-    res.send({'images': files});
-  });
+  if (fs.existsSync('./images')){
+    fs.readdir('./images', (err, files) => {
+      res.send({'images': files});
+    });
+  }
+  else{
+    res.send({'images': []});
+  }
+});
+
+app.get('/api/download/:image', (req, res) => {
+  if(fs.existsSync('./images/'+req.params.image)){
+    res.sendFile(path.resolve(__dirname, 'images', req.params.image));
+  }
+  else{
+    res.send('File not found !');
+  }
 });
 
 app.get('*', (req, res) => {
