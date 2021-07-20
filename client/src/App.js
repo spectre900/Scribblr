e0f80769bbfff3c2aeb15bef327eaf06fbd68b31
spectre900 <nayakpratham0@gmail.com>
2021-07-20 15:05:08 +0530

    final done

--- client/src/App.js
+++ client/src/App.js
@@ -1,110 +1,23 @@
-import React, { Component } from 'react';
-import Excalidraw, { exportToCanvas } from '@excalidraw/excalidraw';
-import axios from 'axios';
-import { v4 } from 'uuid';
 
-import './App.css';
+import short from 'short-uuid';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+
+import ExcalidrawApp from './components/ExcalidrawApp';
 
 class App extends Component {
-
-  constructor(){
-    super();
-    this.id = v4();
-    this.ref = React.createRef();
-    this.state={
-      zenMode: false,
-      viewMode: false,
-      gridMode: false,
-      latestState: [],
-      latestElement: []
-    }
-    this.handleOnChange = this.handleOnChange.bind(this);
-  }
-
-  componentDidMount() {
-    setInterval(
-      ()=>{
-        const canvas = exportToCanvas({
-          elements: this.state.latestElement,
-          appState: this.state.latestState,
-        });
-
-        canvas.toBlob((blob)=>{
-
-          var data = new FormData();
-          data.append('img', blob, this.id+'.jpeg');
-  
-          const config = {
-            headers: { 'Content-Type': 'multipart/form-data' }
-          };
-  
-          axios.post('/api/save', data, config).then(response => {
-            console.log(response.data)
-          });
-        },
-        'image/jpeg',
-        0.5,
-        );
-
-      },
-      10000
-    );
-  }
-
-  handleOnChange(element, state){
-    this.setState({
-      latestElement: element,
-      latestState: state
-    });
-  }
-
   render() {
     return (
-      <div className="holder">
-        <h1> Excalidraw </h1>
+      <Router>
         <div>
-          <button
-            onClick={() => {
-              this.ref.current.resetScene();
-            }}
-          >
-            Reset Scene
-          </button>
-          <label>
-            <input
-              type="checkbox"
-              checked={this.state.viewMode}
-              onChange={() => this.setState({viewMode: !this.state.viewMode})}
-            />
-            View mode
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              checked={this.state.zenMode}
-              onChange={() => this.setState({zenMode: !this.state.zenMode})}
-            />
-            Zen mode
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              checked={this.state.gridMode}
-              onChange={() => this.setState({gridMode: !this.state.gridMode})}
-            />
-            Grid mode
-          </label>
+        <Switch>
+          <Route exact path='/:id' component={ExcalidrawApp}/>
+          <Route path='/'>
+            <Redirect  to={'/'+short.generate()} />
+          </Route>
+        </Switch>
         </div>
-        <div className="drawboard">
-          <Excalidraw
-            ref={this.ref}
-            onChange={this.handleOnChange}
-            viewModeEnabled={this.state.viewMode}
-            zenModeEnabled={this.state.zenMode}
-            gridModeEnabled={this.state.gridMode}
-          />
-        </div>
-      </div>
+      </Router>
     );
   }
 }
---
+++ client/src/components/ExcalidrawApp.js
@@ -0,0 +1,172 @@
+import axios from 'axios';
+import download from 'downloadjs';
+import React, { Component, createRef } from 'react';
+import Excalidraw, { exportToCanvas } from '@excalidraw/excalidraw';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Row, Col, Container, ListGroup, ListGroupItem} from 'react-bootstrap';
+
+import styles from './ExcalidrawApp.module.css';
+
+class ExcalidrawApp extends Component {
+
+  constructor(){
+
+    super();
+    this.state={
+      zenMode: false,
+      viewMode: false,
+      gridMode: true,
+      latestState: [],
+      latestElement: [],
+      images: []
+    }
+    
+    this.ref = createRef();
+    this.id = window.location.href.split('/').pop();
+
+    this.download = this.download.bind(this);
+    this.syncData = this.syncData.bind(this);
+    this.renderItems = this.renderItems.bind(this);
+    this.handleOnChange = this.handleOnChange.bind(this);
+  }
+
+  syncData(){
+    
+    const canvas = exportToCanvas({
+      elements: this.state.latestElement,
+      appState: this.state.latestState,
+    });
+
+    canvas.toBlob((blob)=>{
+
+      var data = new FormData();
+      data.append('img', blob, this.id+'.jpeg');
+
+      const config = {
+        headers: { 'Content-Type': 'multipart/form-data' }
+      };
+
+      axios.post('/api/save', data, config).then(response => {
+        console.log(response.data)
+      });
+    },
+    'image/jpeg',
+    0.5,
+    );
+
+    axios.get('/api/show').then((res)=>{
+      this.setState({
+        images: res.data.images
+      });
+    });
+
+  }
+
+  componentDidMount() {
+    this.syncData();
+    setInterval(this.syncData,10000);
+  }
+
+  handleOnChange(element, state){
+    this.setState({
+      latestElement: element,
+      latestState: state
+    });
+  }
+
+  download(name){
+    fetch('/api/download/'+name).then((res)=>{
+      res.blob().then((blob)=>{
+        download(blob, name);
+      })
+    })
+  }
+
+  renderItems(name, index){
+      return (
+      <ListGroupItem className={styles.item}>
+        {name}
+        <button className={styles.download} onClick={() => this.download(name)} />
+      </ListGroupItem>
+      );
+  }
+
+  render() {
+    return (
+      <Container fluid className={styles.holder}>
+        <Row>
+         <Col xl={8}>
+          <Row>
+            <Col xl={6}>
+              <h1 className={styles.header}>
+                Excalidraw
+              </h1>
+            </Col>
+            <Col xl={6} className={styles.modes}>
+              <button 
+                onClick={() => {this.ref.current.resetScene();}}
+                className={styles.reset_button}
+              >
+                Reset Screen
+              </button>
+              <label className={styles.labels}>
+                <input
+                  type='checkbox'
+                  className={styles.check}
+                  checked={this.state.viewMode}
+                  onChange={() => this.setState({viewMode: !this.state.viewMode})}
+                />
+                View mode
+              </label>
+              <label className={styles.labels}>
+                <input
+                  type='checkbox'
+                  className={styles.check}
+                  checked={this.state.zenMode}
+                  onChange={() => this.setState({zenMode: !this.state.zenMode})}
+                />
+                Zen  mode
+              </label>
+              <label className={styles.labels}>
+                <input
+                  type='checkbox'
+                  className={styles.check}
+                  checked={this.state.gridMode}
+                  onChange={() => this.setState({gridMode: !this.state.gridMode})}
+                />
+                Grid mode
+              </label>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+            <div className={styles.drawboard}>
+              <Excalidraw
+                ref={this.ref}
+                onChange={this.handleOnChange}
+                viewModeEnabled={this.state.viewMode}
+                zenModeEnabled={this.state.zenMode}
+                gridModeEnabled={this.state.gridMode}
+              />
+            </div>
+            </Col>
+          </Row>
+         </Col>
+         <Col xl={4}>
+            <div className={styles.list}>
+              <h1 className={styles.title}>
+                Saved Images
+              </h1>
+              <ListGroup>
+                {this.state.images.map(this.renderItems)}
+              </ListGroup>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default ExcalidrawApp;
